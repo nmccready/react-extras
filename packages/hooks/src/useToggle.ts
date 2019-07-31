59e8830ee0a278852e6ed46a832e6eed7b8f0335
nmccready <nemtcan@gmail.com>
2019-07-31 18:08:54 -0400

    fix: debug.js path fix

--- packages/hooks/src/useToggle.ts
+++ packages/hooks/src/useToggle.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const debug = require('../../../debug').spawn('react:hooks:useToggle');
+const debug = require('../debug').spawn('react:hooks:useToggle');
 
 export interface UseToggleStates {
   one: any;
