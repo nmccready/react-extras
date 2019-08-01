ff905e04f059009c33c9ad9b0fb4c27ce6c9503c
nmccready <nemtcan@gmail.com>
2019-08-01 15:35:16 -0400

    fix(useGlobalHook): state field on Store type now exists and is more flexible as a generic

--- packages/hooks/src/useGlobalHook/useGlobalHook.d.ts
+++ packages/hooks/src/useGlobalHook/useGlobalHook.d.ts
@@ -8,35 +8,36 @@ interface ReactLib {
 }
 
 /* since use-global-hook is not ts yet, might move this to it's index.d.ts file */
-export type SetStateFn = (newState: any, isRef?: boolean) => void;
+export type SetStateFn = <T extends any>(newState: T, isRef?: boolean) => void;
 export declare const setState: SetStateFn;
-export type SetRefFn = (newState: any) => void;
+export type SetRefFn = <T extends any>(newState: T) => void;
 export declare const setRef: SetRefFn;
 
 export type UseCustomFn = (React: ReactLib) => void;
 export declare const useCustom: UseCustomFn;
 
-export type Action = (store: Store, ..._: any) => any | void;
+export type Action = <T extends any>(store: Store<T>, ..._: any) => any | void;
 export type Actions = Map<Action>;
 
-export type AssociateActionsFn = (store: Store, actions: Actions) => Actions;
+export type AssociateActionsFn = <T extends any>(store: Store<T>, actions: Actions) => Actions;
 export declare const associateActions: AssociateActionsFn;
 
-export type InitializerFn = (_: Store) => void;
+export type InitializerFn = <T extends any>(_: Store<T>) => void;
 
-export type UseStoreFn = (
+export type UseStoreFn = <T extends any>(
   React: ReactLib,
-  initialState: any,
+  initialState: T,
   actions: Actions,
   initializer?: InitializerFn
 ) => UseCustomFn;
 
 export declare const useStore: UseStoreFn;
 
-export interface Store {
+export interface Store<T> {
   setState: SetStateFn;
   setRef: SetRefFn;
   actions: Actions;
+  state: T;
 }
 
 export default useStore;
