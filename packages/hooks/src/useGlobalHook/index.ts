c442245a2470d5d116fc6574182b69e851adf9d7
nmccready <nemtcan@gmail.com>
2019-08-21 17:34:22 -0400

    optional WorkR type on use-global-hook

--- packages/hooks/src/useGlobalHook/index.ts
+++ packages/hooks/src/useGlobalHook/index.ts
@@ -3,14 +3,14 @@ import $useGlobalHook, { Initializer } from '@znemz/use-global-hook';
 
 import { InnerBaseActions } from '@znemz/use-global-hook/lib/actions';
 
-export interface OurUseStoreProps<T, InnerA, OuterA, WorkR> {
+export interface OurUseStoreProps<T, InnerA, OuterA, WorkR = undefined> {
   initialState?: T;
   actions?: InnerA | InnerBaseActions<T>;
   initializer?: Initializer<T, OuterA>;
   hookWork?: () => WorkR;
 }
 
-export const useGlobalHook = <T, InnerA, OuterA, WorkR>({
+export const useGlobalHook = <T, InnerA, OuterA, WorkR = undefined>({
   initialState,
   actions,
   initializer,
