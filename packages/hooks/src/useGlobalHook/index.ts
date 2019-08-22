659e29ad051928264fe0c72f1afecc1c5b0c1d22
nmccready <nemtcan@gmail.com>
2019-08-22 02:11:03 -0400

    bump use-global-hook

--- packages/hooks/src/useGlobalHook/index.ts
+++ packages/hooks/src/useGlobalHook/index.ts
@@ -1,25 +1,31 @@
 import { useState, useEffect } from 'react';
 import $useGlobalHook, { Initializer } from '@znemz/use-global-hook';
 
-import { InnerBaseActions } from '@znemz/use-global-hook/lib/actions';
+import { InnerBaseActions, OuterBaseActions } from '@znemz/use-global-hook/lib/actions';
 
-export interface OurUseStoreProps<T, InnerA, OuterA, WorkR = undefined> {
+export interface OurUseStoreProps<
+  T,
+  InnerA = InnerBaseActions<T>,
+  OuterA = OuterBaseActions<T>
+> {
   initialState?: T;
   actions?: InnerA | InnerBaseActions<T>;
   initializer?: Initializer<T, OuterA>;
-  hookWork?: () => WorkR;
 }
 
-export const useGlobalHook = <T, InnerA, OuterA, WorkR = undefined>({
+export const useGlobalHook = <
+  T,
+  InnerA = InnerBaseActions<T>,
+  OuterA = OuterBaseActions<T>,
+  WorkR = undefined
+>({
   initialState,
   actions,
   initializer,
-  hookWork,
-}: OurUseStoreProps<T, InnerA, OuterA, WorkR>) =>
-  $useGlobalHook({
+}: OurUseStoreProps<T, InnerA, OuterA>) =>
+  $useGlobalHook<T, InnerA, OuterA, WorkR>({
     React: { useState, useEffect },
     initialState,
     actions,
     initializer,
-    hookWork,
   });
