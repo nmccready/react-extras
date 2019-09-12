6c43387c89007ac8ca1c665b4f19e68a6bce7e4c
nmccready <nemtcan@gmail.com>
2019-09-12 11:16:09 -0400

    small change in reinspect story for global hook

--- packages/storybook/src/useGlobalHook/useGlobalHook.story.js
+++ packages/storybook/src/useGlobalHook/useGlobalHook.story.js
@@ -10,6 +10,10 @@ to use-global-hook
 */
 storiesOf('global hooks', module).add('counter', () => (
   <StateInspector name="Counter Example">
+    <div>
+      With reducerId not being defined when reinspect.useState is injected to use-global-hook;
+      will instead proxy to React.useState
+    </div>
     <CounterProvider />
   </StateInspector>
 ));
